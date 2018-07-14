aabdec9f175cd35b925406b92a1f9e1af8cf6877
taljacobson <tal1989@gmail.com>
2018-07-15 00:52:31 +0300

    add ios code. - not working yet add pubspec info

--- ios/Classes/FlutterMailerPlugin.m
+++ ios/Classes/FlutterMailerPlugin.m
@@ -1,4 +1,6 @@
 #import "FlutterMailerPlugin.h"
+#import <UIKit/UIKit.h>
+#import <MessageUI/MessageUI.h>
 
 @implementation FlutterMailerPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
@@ -10,11 +12,158 @@
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"getPlatformVersion" isEqualToString:call.method]) {
-    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  if ([@"send" isEqualToString:call.method]) {
+    // result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+    [self send:call.arguments:result];
   } else {
     result(FlutterMethodNotImplemented);
   }
 }
+
+- (void)send:(mail:(NSDictionary *)options
+                  callback: (FlutterResult)callback)
+{
+    if ([MFMailComposeViewController canSendMail])
+    {
+        MFMailComposeViewController *mail = [[MFMailComposeViewController alloc] init];
+        mail.mailComposeDelegate = self;
+        
+
+        if (options[@"subject"]){
+            NSString *subject = [ NSString:options[@"subject"]];
+            [mail setSubject:subject];
+        }
+        
+        bool *isHTML = NO;
+        
+        if (options[@"isHTML"]){
+            isHTML = [options[@"isHTML"] boolValue];
+        }
+
+        if (options[@"body"]){
+            NSString *body = [ NSString:options[@"body"]];
+            [mail setMessageBody:body isHTML:isHTML];
+        }
+
+        if (options[@"recipients"]){
+            NSArray *recipients = [ NSArray:options[@"recipients"]];
+            [mail setToRecipients:recipients];
+        }
+
+        if (options[@"ccRecipients"]){
+            NSArray *ccRecipients = [ NSArray:options[@"ccRecipients"]];
+            [mail setCcRecipients:ccRecipients];
+        }
+        
+        if (options[@"bccRecipients"]){
+            NSArray *bccRecipients = [NSArray:options[@"bccRecipients"]];
+            [mail setBccRecipients:bccRecipients];
+        }
+
+        if (options[@"attachment"]){
+            NSArray *attachments = [ NSArray:options[@"attachments"]];
+            for (NSString *attachmentPath in attachments) {
+              NSLog(@"Single element: %@", attachmentPath);
+            
+            
+              NSString *attachmentType = [[attachmentPath lastPathComponent] pathExtension];
+              // Get the resource path and read the file using NSData
+              NSData *fileData = [NSData dataWithContentsOfFile:attachmentPath];
+              // set the name of the file
+              NSString *attachmentName = [[attachmentPath lastPathComponent] stringByDeletingPathExtension];
+              // Determine the MIME type
+              NSString *mimeType;
+              
+              /*
+              * Add additional mime types and PR if necessary. Find the list
+              * of supported formats at http://www.iana.org/assignments/media-types/media-types.xhtml
+              */
+              if ([attachmentType isEqualToString:@"jpg"]) {
+                  mimeType = @"image/jpeg";
+              } else if ([attachmentType isEqualToString:@"png"]) {
+                  mimeType = @"image/png";
+              } else if ([attachmentType isEqualToString:@"doc"]) {
+                  mimeType = @"application/msword";
+              } else if ([attachmentType isEqualToString:@"ppt"]) {
+                  mimeType = @"application/vnd.ms-powerpoint";
+              } else if ([attachmentType isEqualToString:@"html"]) {
+                  mimeType = @"text/html";
+              } else if ([attachmentType isEqualToString:@"csv"]) {
+                  mimeType = @"text/csv";
+              } else if ([attachmentType isEqualToString:@"pdf"]) {
+                  mimeType = @"application/pdf";
+              } else if ([attachmentType isEqualToString:@"vcard"]) {
+                  mimeType = @"text/vcard";
+              } else if ([attachmentType isEqualToString:@"json"]) {
+                  mimeType = @"application/json";
+              } else if ([attachmentType isEqualToString:@"zip"]) {
+                  mimeType = @"application/zip";
+              } else if ([attachmentType isEqualToString:@"text"]) {
+                  mimeType = @"text/*";
+              } else if ([attachmentType isEqualToString:@"mp3"]) {
+                  mimeType = @"audio/mpeg";
+              } else if ([attachmentType isEqualToString:@"wav"]) {
+                  mimeType = @"audio/wav";
+              } else if ([attachmentType isEqualToString:@"aiff"]) {
+                  mimeType = @"audio/aiff";
+              } else if ([attachmentType isEqualToString:@"flac"]) {
+                  mimeType = @"audio/flac";
+              } else if ([attachmentType isEqualToString:@"ogg"]) {
+                  mimeType = @"audio/ogg";
+              } else if ([attachmentType isEqualToString:@"xls"]) {
+                  mimeType = @"application/vnd.ms-excel";     
+              }
+
+              // Add attachment
+                [mail addAttachmentData:fileData mimeType:mimeType fileName:attachmentName];
+            }
+        }
+
+        UIViewController *root = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
+
+        while (root.presentedViewController) {
+            root = root.presentedViewController;
+        }
+        [root presentViewController:mail animated:YES completion:nil];
+    } else {
+        callback(@[@"not_available"]);
+    }
+}
+
+#pragma mark MFMailComposeViewControllerDelegate Methods
+
+- (void)mailComposeController:(MFMailComposeViewController *)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError *)error
+{
+    // NSString *key = RCTKeyForInstance(controller);
+    // RCTResponseSenderBlock callback = _callbacks[key];
+    // if (callback) {
+    //     switch (result) {
+    //         case MFMailComposeResultSent:
+    //             callback(@[[NSNull null] , @"sent"]);
+    //             break;
+    //         case MFMailComposeResultSaved:
+    //             callback(@[[NSNull null] , @"saved"]);
+    //             break;
+    //         case MFMailComposeResultCancelled:
+    //             callback(@[[NSNull null] , @"cancelled"]);
+    //             break;
+    //         case MFMailComposeResultFailed:
+    //             callback(@[@"failed"]);
+    //             break;
+    //         default:
+    //             callback(@[@"error"]);
+    //             break;
+    //     }
+        // [_callbacks removeObjectForKey:key];
+    } else {
+        NSLogWarn(@"No callback registered for mail: %@", controller.title);
+    }
+    UIViewController *ctrl = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
+    while (ctrl.presentedViewController && ctrl != controller) {
+        ctrl = ctrl.presentedViewController;
+    }
+    [ctrl dismissViewControllerAnimated:YES completion:nil];
+}
+
 
 @end

f1e83186711371ca9d6e422b80fad246c79152a7
Tal Jacobson <tal1989@gmail.com>
2021-03-05 13:37:35 +0200

    recreate example project

---
+++ example/android/app/src/main/java/com/dataxad/flutter_mailer_example/MainActivity.java
@@ -0,0 +1,6 @@
+package com.dataxad.flutter_mailer_example;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- example/test_driver/flutter_mailer_e2e.dart
+++ example/test_driver/flutter_mailer_e2e.dart
@@ -1,9 +1,9 @@
 import 'package:flutter_mailer/flutter_mailer.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:e2e/e2e.dart';
+import 'package:integration_test/integration_test.dart';
 
 void main() {
-  E2EWidgetsFlutterBinding.ensureInitialized();
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   // works but opens an external app and needs to come back into the app to succeed.
   testWidgets('can open mail', (WidgetTester tester) async {
